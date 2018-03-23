185652f6e9fd9c31d4607aa5c8d541717d8c0fcd
Jean717p <jeanpi.francois@gmail.com>
2018-03-23 12:36:37 +0100

    Edit button on mainactivity

--- app/src/main/java/com/mad18/nullpointerexception/takeabook/MainActivity.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/MainActivity.java
@@ -1,9 +1,14 @@
 package com.mad18.nullpointerexception.takeabook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +23,11 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_inbox, menu); //.xml file name
         return super.onCreateOptionsMenu(menu);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent = new Intent(MainActivity.this,EditActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
 }
