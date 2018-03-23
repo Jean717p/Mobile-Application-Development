973f12d789710a9e4bf7806c29dfffacee42ce20
Valeria Sottile <valeria.sot93@gmail.com>
2018-03-23 10:00:40 +0100

    aggiunta della matita

--- app/src/main/java/com/mad18/nullpointerexception/takeabook/MainActivity.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lactivity_main);
+        setContentView(R.layout.show_profile);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
