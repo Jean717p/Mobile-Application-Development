9224a91bc700bb7a61dc503b34a0a27dc415bbb1
Jean717p <jeanpi.francois@gmail.com>
2018-03-24 19:57:03 +0100

    Memorizza i dati dalle textview e li mostra correttamente. Aggiunta tasto indietro rispetto all'activity

--- app/src/main/java/com/mad18/nullpointerexception/takeabook/EditActivity.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/EditActivity.java
@@ -1,37 +1,86 @@
 package com.mad18.nullpointerexception.takeabook;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
-
+    private SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_profile);
+        sharedPref = this.
+                getSharedPreferences(getString(R.string.app_name),Context.MODE_PRIVATE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_inbox, menu); //.xml file name
+        inflater.inflate(R.menu.edit_profile, menu); //.xml file name
         return super.onCreateOptionsMenu(menu);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId() == R.id.action_edit){
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_save:
+                storeUserEditData();
+                finish();
+                break;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                break;
         }
-        else{
-            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fillUserEditData();
+    }
+
+    private void storeUserEditData(){
+        EditText text;
+        SharedPreferences.Editor editor = sharedPref.edit();
+        text = (EditText) findViewById(R.id.profile_username);
+        if(text.getText().toString().isEmpty()==false) {
+            String x = text.getText().toString();
+            editor.putString("user_name", text.getText().toString());
         }
-        return true;
+        text = (EditText) findViewById(R.id.profile_city);
+        if(text.getText().toString().isEmpty()==false) {
+            editor.putString("user_city", text.getText().toString());
+        }
+        text = (EditText) findViewById(R.id.profile_mail);
+        if(text.getText().toString().isEmpty()==false) {
+            editor.putString("user_mail", text.getText().toString());
+        }
+        text = (EditText) findViewById(R.id.profile_about);
+        if(text.getText().toString().isEmpty()==false){
+                //&& t.getText().toString().length()<600) {
+            editor.putString("user_about", text.getText().toString());
+        }
+        editor.apply();
+    }
+
+    private void fillUserEditData(){
+        EditText inputText;
+        inputText = (EditText) findViewById(R.id.profile_username);
+        inputText.setText(sharedPref.getString("user_name", ""));
+        inputText = (EditText) findViewById(R.id.profile_city);
+        inputText.setText(sharedPref.getString("user_city", ""));
+        inputText = (EditText) findViewById(R.id.profile_mail);
+        inputText.setText(sharedPref.getString("user_mail", ""));
+        inputText = (EditText) findViewById(R.id.profile_about);
+        inputText.setText(sharedPref.getString("user_about",""));
     }
 
 }
--- app/src/main/java/com/mad18/nullpointerexception/takeabook/MainActivity.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/MainActivity.java
@@ -1,21 +1,24 @@
 package com.mad18.nullpointerexception.takeabook;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toolbar;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences sharedPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_profile);
+        sharedPref = getSharedPreferences(getString(R.string.app_name),Context.MODE_PRIVATE);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,5 +32,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         return true;
     }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        fillUserData();
+    }
 
+    private void fillUserData(){
+        TextView text = findViewById(R.id.Username);
+        text.setText(sharedPref.getString("user_name", getString(R.string.name)));
+        text = (TextView) findViewById(R.id.City);
+        text.setText(sharedPref.getString("user_city", getString(R.string.profile_city)));
+        text = (TextView) findViewById(R.id.profile_mail);
+        text.setText(sharedPref.getString("user_mail", getString(R.string.profile_mail)));
+        text = (TextView) findViewById(R.id.profile_AboutMe);
+        text.setText(sharedPref.getString("user_about", getString(R.string.profile_about)));
+    }
 }
