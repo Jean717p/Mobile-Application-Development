fc496172fa6bd97c48c2cde2f7bdad9039b5a067
Jean717p <jeanpi.francois@gmail.com>
2018-03-26 15:46:24 +0200

    Ritorno a due activity separate

--- app/src/main/java/com/mad18/nullpointerexception/takeabook/showProfile.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/showProfile.java
@@ -1,6 +1,7 @@
 package com.mad18.nullpointerexception.takeabook;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,7 +17,6 @@ public class showProfile extends AppCompatActivity {
     private int editTextBoxesIds[] = new int[]{R.id.Username,R.id.City,
             R.id.profile_about,R.id.profile_mail};
     private Menu menu;
-    private boolean editMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +27,20 @@ public class showProfile extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(R.string.app_name);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        if(savedInstanceState == null){
-            this.editMode = false;
-            fillUserData();
-        }
-        else{
-            if(savedInstanceState.getString("editMode","false").equals("true")){
-                this.editMode = true;
-            }
-        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fillUserData();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        this.menu=menu;
+        this.menu = menu;
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_inbox, menu); //.xml file name
-        if(this.editMode){
-            goToEditMode();
-        }
+        goToViewMode();
         return true;
     }
 
@@ -53,13 +48,8 @@ public class showProfile extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case R.id.action_settings:
-                if(this.editMode ==true){
-                    storeUserEditData();
-                    onBackPressed();
-                }
-                else{
-                    goToEditMode();
-                }
+                Intent intent = new Intent(this,editProfile.class);
+                startActivity(intent);
                 return true;
 
             case android.R.id.home:
@@ -74,37 +64,6 @@ public class showProfile extends AppCompatActivity {
         for(int i:editTextBoxesIds){
             text = findViewById(i);
             text.setText(sharedPref.getString(Integer.toString(i),""));
-        }
-    }
-
-    private void storeUserEditData(){
-        EditText text;
-        SharedPreferences.Editor editor = sharedPref.edit();
-        for(int i: editTextBoxesIds){
-            text = findViewById(i);
-            editor.putString(Integer.toString(i),text.getText().toString());
-        }
-        editor.apply();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        EditText text;
-        for(int i: editTextBoxesIds){
-            text = findViewById(i);
-            outState.putString(Integer.toString(i),text.getText().toString());
-        }
-        outState.putString("editMode",Boolean.toString(this.editMode));
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        EditText text;
-        for(int i: editTextBoxesIds){
-            text = findViewById(i);
-            text.setText(savedInstanceState.getString(Integer.toString(i),""));
         }
     }
 
@@ -125,49 +84,6 @@ public class showProfile extends AppCompatActivity {
         for(int i:editTextBoxesIds){
             findViewById(i).setEnabled(false);
         }
-        fillUserData();
-        this.editMode = false;
     }
-
-    private void goToEditMode(){
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        changeIcon(R.drawable.ic_done_white_48dp);
-        for(int i: editTextBoxesIds){
-            findViewById(i).setEnabled(true);
-        }
-        this.editMode = true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(this.editMode){
-            goToViewMode();
-        }
-        else{
-            super.onBackPressed();
-        }
-    }
-
-    /***** To Do */
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        if (resultCode == RESULT_OK) {
-//            if (requestCode == PICK_IMAGE) {
-//                Uri selectedMediaUri = data.getData();
-//                if (selectedMediaUri.toString().contains("image")) {
-//                    //handle image  -- To Do salvare immagine e settarla come immagine
-//                }
-//            }
-//        }
-//    }
-//
-//    private void selectUserImg(){
-//        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-//        getIntent.setType("image/*");
-//        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//        pickIntent.setType("image/*");
-//        Intent chooserIntent = Intent.createChooser(getIntent,"Select Image");
-//        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
-//        startActivityForResult(chooserIntent, PICK_IMAGE);
-//    }
 
 }
