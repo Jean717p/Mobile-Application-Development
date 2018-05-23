0c2fad65ad8100e60c50132e1c40a27d09aea187
Simone Leonardi <simone.leonardi.rd@gmail.com>
2018-05-23 16:34:03 +0200

    userwrapper con anche geopoint

--- app/src/main/java/com/mad18/nullpointerexception/takeabook/UserWrapper.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/UserWrapper.java
@@ -3,12 +3,10 @@ package com.mad18.nullpointerexception.takeabook;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import com.google.firebase.firestore.GeoPoint;
-import com.mad18.nullpointerexception.takeabook.addBook.BookWrapper;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+
 
 public class UserWrapper implements Parcelable{
 
@@ -20,6 +18,7 @@ public class UserWrapper implements Parcelable{
     List<String> user_wrapper_books;
     //GeoPoint user_wrapper_geoPoint;
     String user_wrapper_profileImgStoragePath;
+    private double user_wrapper_lat, user_wrapper_longitude;
 
     public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
         public UserWrapper createFromParcel(Parcel in) {
@@ -36,6 +35,7 @@ public class UserWrapper implements Parcelable{
         return 0;
     }
 
+
     @Override
     public void writeToParcel(Parcel parcel, int i) {
             parcel.writeString(this.user_wrapper_name);
@@ -45,18 +45,23 @@ public class UserWrapper implements Parcelable{
         parcel.writeString(this.user_wrapper_id);
         parcel.writeStringList(this.user_wrapper_books);
         parcel.writeString(this.user_wrapper_profileImgStoragePath);
+        parcel.writeDouble(this.user_wrapper_lat);
+        parcel.writeDouble(this.user_wrapper_longitude);
+
     }
 
     public UserWrapper(String user_wrapper_name, String user_wrapper_city, String user_wrapper_mail,
-                       String user_wrapper_about, String user_wrapper_id, List<String> user_wrapper_books, String user_wrapper_profileImgStoragePath) {
+                       String user_wrapper_about, String user_wrapper_id, List<String> user_wrapper_books, String user_wrapper_profileImgStoragePath,
+                       Double lat, Double longitude) {
         this.user_wrapper_name = user_wrapper_name;
         this.user_wrapper_city = user_wrapper_city;
         this.user_wrapper_mail = user_wrapper_mail;
         this.user_wrapper_about = user_wrapper_about;
         this.user_wrapper_id = user_wrapper_id;
         this.user_wrapper_books = user_wrapper_books;
-        //this.user_wrapper_geoPoint = user_wrapper_geoPoint;
         this.user_wrapper_profileImgStoragePath = user_wrapper_profileImgStoragePath;
+        this.user_wrapper_lat = lat;
+        this.user_wrapper_longitude = longitude;
     }
 
     public UserWrapper(User user){
@@ -66,8 +71,9 @@ public class UserWrapper implements Parcelable{
         this.user_wrapper_about = user.getUsr_about();
         this.user_wrapper_id = user.getUsr_id();
         this.user_wrapper_books = new LinkedList<>(user.getUsr_books().keySet());
-        //this.user_wrapper_geoPoint = user.getUsr_geoPoint();
         this.user_wrapper_profileImgStoragePath = user.getProfileImgStoragePath();
+        this.user_wrapper_lat = user.getUsr_geoPoint().getLatitude();
+        this.user_wrapper_longitude = user.getUsr_geoPoint().getLongitude();
     }
 
     public UserWrapper(Parcel in){
@@ -79,6 +85,8 @@ public class UserWrapper implements Parcelable{
         this.user_wrapper_books = in.createStringArrayList();
         //this.user_wrapper_geoPoint = in.re;
         this.user_wrapper_profileImgStoragePath = in.readString();
+        this.user_wrapper_lat = in.readDouble();
+        this.user_wrapper_longitude = in.readDouble();
     }
 
     @Override
@@ -98,6 +106,8 @@ public class UserWrapper implements Parcelable{
 
                 Totbooks+
                 ", imgStrgPath='" + user_wrapper_profileImgStoragePath + '\'' +
+                ", lat='" + Double.toString(user_wrapper_lat) + '\'' +
+                ", longitude='" + Double.toString(user_wrapper_longitude) + '\'' +
                 '}';
     }
 
@@ -149,14 +159,6 @@ public class UserWrapper implements Parcelable{
         this.user_wrapper_books = user_wrapper_books;
     }
 
-//    public GeoPoint getUser_wrapper_geoPoint() {
-//        return user_wrapper_geoPoint;
-//    }
-//
-//    public void setUser_wrapper_geoPoint(GeoPoint user_wrapper_geoPoint) {
-//        this.user_wrapper_geoPoint = user_wrapper_geoPoint;
-//    }
-
     public String getUser_wrapper_profileImgStoragePath() {
         return user_wrapper_profileImgStoragePath;
     }
@@ -164,6 +166,20 @@ public class UserWrapper implements Parcelable{
     public void setUser_wrapper_profileImgStoragePath(String user_wrapper_profileImgStoragePath) {
         this.user_wrapper_profileImgStoragePath = user_wrapper_profileImgStoragePath;
     }
+    public double getUser_wrapper_lat() {
+        return user_wrapper_lat;
+    }
 
+    public void setUser_wrapper_lat(double user_wrapper_lat) {
+        this.user_wrapper_lat = user_wrapper_lat;
+    }
+
+    public double getUser_wrapper_longitude() {
+        return user_wrapper_longitude;
+    }
+
+    public void setUser_wrapper_longitude(double user_wrapper_longitude) {
+        this.user_wrapper_longitude = user_wrapper_longitude;
+    }
 
 }
