acd2c587d9a63719aef6963de940202c9fe8c382
Jean717p <jeanpi.francois@gmail.com>
2018-05-31 17:13:56 +0200

    Fix info book -- myBooks

--- app/src/main/java/com/mad18/nullpointerexception/takeabook/info/InfoBook.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/info/InfoBook.java
@@ -21,7 +21,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -79,19 +78,13 @@ public class InfoBook extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(R.string.title_activity_info_book);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //setTitle(R.string.title_activity_info_book);
-
-        toolbar.setVisibility(View.VISIBLE);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         bookToShowInfoOf = getIntent().getExtras().getParcelable("bookToShow");
         sharedPref = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
         fillInfoBookViews();
-
-
     }
 
     private void fillInfoBookViews() {
-
         TextView tv;
         tv = findViewById(R.id.info_book_title);
         tv.setText(bookToShowInfoOf.getTitle());
@@ -103,14 +96,12 @@ public class InfoBook extends AppCompatActivity {
         }
         tv = findViewById(R.id.info_book_ISBN);
         tv.setText(bookToShowInfoOf.getISBN());
-
         tv = findViewById(R.id.info_book_editionYear);
         if (bookToShowInfoOf.getEditionYear() == 0) {
             tv.setText(R.string.add_book_info_not_available);
         } else {
             tv.setText(Integer.toString(bookToShowInfoOf.getEditionYear()));
         }
-
         tv = findViewById(R.id.info_book_pages);
         if (bookToShowInfoOf.getPages() == 0) {
             tv.setText(R.string.add_book_info_not_available);
@@ -118,34 +109,27 @@ public class InfoBook extends AppCompatActivity {
             tv.setText(Integer.toString(bookToShowInfoOf.getPages()));
 
         }
-
         tv = findViewById(R.id.info_book_description);
         if (bookToShowInfoOf.getDescription().length() == 0) {
             tv.setText(getString(R.string.add_book_no_description));
         } else {
             tv.setText(bookToShowInfoOf.getDescription());
         }
-
         tv = findViewById(R.id.info_book_publisher);
         if (bookToShowInfoOf.getPublisher().length() == 0) {
             tv.setText(getString(R.string.add_book_info_not_available));
         } else {
             tv.setText(bookToShowInfoOf.getPublisher());
         }
-
-
         tv = findViewById(R.id.info_book_categories);
         tmp = bookToShowInfoOf.getCategories().toString();
         if (tmp.length() > 2) {
             tv.setText(tmp.substring(1, tmp.length() - 1));
         }
-
         ImageView iw = findViewById(R.id.info_book_main_image);
         Book book = new Book(bookToShowInfoOf);
-
         tv = findViewById(R.id.info_book_book_conditions);
         switch (book.getBook_condition()) {
-
             case 0:
                 tv.setText(getString(R.string.add_book_info_not_available));
                 break;
@@ -159,30 +143,21 @@ public class InfoBook extends AppCompatActivity {
                 tv.setText(getResources().getStringArray(R.array.book_conditions)[3]);
                 break;
         }
-        Glide.with(this).load(book.getBook_thumbnail_url()).into(iw);
-
-
-        //simo inizio
-
+        GlideApp.with(this).load(book.getBook_thumbnail_url())
+                .placeholder(R.drawable.ic_thumbnail_cover_book).into(iw);
         horizontal_photo_list = (LinearLayout) findViewById(R.id.info_book_list_photo_container);
-
         for_me = new LinkedList<>(book.getBook_photo_list().keySet());
         for (int i = 0; i < book.getBook_photo_list().size(); i++) {
             horizontal_photo_list_element = getLayoutInflater().inflate(R.layout.cell_in_image_list, null);
             ImageView imageView = (ImageView) horizontal_photo_list_element.findViewById(R.id.image_in_horizontal_list_cell);
             horizontal_photo_list.addView(horizontal_photo_list_element);
             StorageReference mImageRef = FirebaseStorage.getInstance().getReference(for_me.get(i));
-
-            GlideApp.with(this).load(mImageRef).into(imageView);
-
+            GlideApp.with(this).load(mImageRef)
+                    .placeholder(R.drawable.ic_thumbnail_cover_book).into(imageView);
             ImageViewPopUpHelper.enablePopUpOnClick(InfoBook.this,imageView);
-
         }
-        //simo fine
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference user_doc;
-
         user_doc = db.collection("users").document(bookToShowInfoOf.getUser_id());
         user_doc.get().addOnCompleteListener(task -> {
             DocumentSnapshot doc = task.getResult();
@@ -192,42 +167,37 @@ public class InfoBook extends AppCompatActivity {
                 return;
             }
             bookOwner = doc.toObject(User.class);
-            bookOwnerWrapped = new UserWrapper(bookOwner);
             TextView tv2 = findViewById(R.id.info_book_owner);
             mAuth = FirebaseAuth.getInstance();
             FirebaseUser user = mAuth.getCurrentUser();
+            final Button request_button = findViewById(R.id.info_book_request_book_button);
             if(!bookToShowInfoOf.getUser_id().equals(user.getUid())) {
-                final Button request_button = findViewById(R.id.info_book_request_book_button);
                 request_button.setClickable(true);
                 request_button.setVisibility(View.VISIBLE);
                 request_button.setOnClickListener( (View view) -> {
                     //TODO: richiesta libro
                     Intent toRequestBook = new Intent(InfoBook.this, RequestBook.class);
                     toRequestBook.putExtra("requested_book", bookToShowInfoOf);
-                    toRequestBook.putExtra("otherUser",bookOwnerWrapped);
+                    toRequestBook.putExtra("otherUser",new UserWrapper(bookOwner));
                     startActivity(toRequestBook);
                 });
                 tv2.setText(bookOwner.getUsr_name());
                 tv2.setTextColor(Color.BLUE);
                 tv2.setClickable(true);
-                UserWrapper userWrapper = new UserWrapper(bookOwner);
                 tv2.setOnClickListener(view -> {
                     Intent toInfoUser = new Intent(InfoBook.this, InfoUser.class);
-                    toInfoUser.putExtra("otherUser", userWrapper);
-                    //toInfoUser.putExtra("img_uri",downloadOwnerUri);
-                    //qui l'immagine
-
+                    toInfoUser.putExtra("otherUser", new UserWrapper(bookOwner));
                     startActivity(toInfoUser);
                 });
             }
             else{
-                tv2.setVisibility(View.GONE);
-                //tv2.setHeight(0);
+                tv2.setVisibility(View.INVISIBLE);
+                tv2.setHeight(0);
                 tv2 = findViewById(R.id.info_book_label_owner);
-                tv2.setVisibility(View.GONE);
-                //tv2.setHeight(0);
+                tv2.setHeight(0);
+                tv2.setVisibility(View.INVISIBLE);
+                request_button.setVisibility(View.GONE);
             }
-
         });
     }
 
@@ -238,7 +208,6 @@ public class InfoBook extends AppCompatActivity {
                 .setTitle(R.string.info_book_delete_this_book)
                 .setMessage(R.string.info_book_delete_want_delete)
                 .setIcon(R.drawable.ic_delete_white_24px)
-
                 .setPositiveButton(R.string.info_book_delete_this_book, (dialog, whichButton) -> {
                     //your deleting code
                     DeleteBook();
@@ -251,9 +220,7 @@ public class InfoBook extends AppCompatActivity {
     }
 
     private void DeleteBook() {
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         db.collection("books").document(bookToShowInfoOf.getISBN() + bookToShowInfoOf.getUser_id())
                 .delete().addOnSuccessListener(new OnSuccessListener< Void >() {
             @Override
@@ -280,9 +247,7 @@ public class InfoBook extends AppCompatActivity {
                         StorageReference mImageRef = FirebaseStorage.getInstance().getReference(for_me.get(i));
                         mImageRef.delete().addOnSuccessListener(aVoid1 -> {
                             // File deleted successfully
-
                             Log.d("delete", "deleted photo");
-
                             Intent bookRemovedintent = new Intent();
                             bookRemovedintent.putExtra("book_removed", bookToShowInfoOf);
                             setResult(BOOK_EFFECTIVELY_REMOVED,bookRemovedintent);
--- app/src/main/java/com/mad18/nullpointerexception/takeabook/requestBook/RequestRecyclerViewAdapter.java
+++ app/src/main/java/com/mad18/nullpointerexception/takeabook/requestBook/RequestRecyclerViewAdapter.java
@@ -107,8 +107,6 @@ class RequestRecyclerViewAdapter extends RecyclerView.Adapter<RequestRecyclerVie
 
                 }
             });
-
-
         }
 
     }
